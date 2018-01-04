3973eb9c9df2d4bae69301c1a8d91a9ffa5ae750
Mark Lyen <mark@marklyen.com>
2018-01-03 19:33:49 -0800

    Initial Code Load.

---
+++ BatteryDisplay/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteryDisplay
+//
+//  Created by Mark Lyen on 1/3/18.
+//  Copyright © 2018 _MarkLyen_. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
